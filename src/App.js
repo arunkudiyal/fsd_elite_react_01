f26ab25f254bfaf7a32bdad07d071a9ffee687f8
arunkudiyal <arunkudiyal@icloud.com>
2022-05-30 14:55:10 +0530

    State in Function-Based Components & Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       { name: 'Daniel', age: '27' },
       { name: 'Tony', age: '32 ' }
     ],
-    someOtherStateValue: 'This is an another state value'
+    someOtherStateValue: 'This is an another state value',
+    showPersons: false
   }
 
   switchNameHandler = () => {
@@ -24,18 +25,27 @@ class App extends Component {
     })
   }
 
+  togglePersonHandler = () => {
+    const currentShowValue = this.state.showPersons
+    this.setState({ showPersons: !currentShowValue })
+  }
+
   render() {
     return (
       <div>
         <div className="container-fluid">
-          <h1 className="page-header">Person Management</h1>
+          <h1 className="page-header">Person Management</h1> <hr />
+          <button onClick={this.togglePersonHandler} className='btn btn-warning'>Toggle Persons</button> <hr />
         </div>
-        <div className="container">
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} /> <hr />
-          <button onClick={this.switchNameHandler} className='btn btn-primary'>Switch Name</button>
-        </div>
+        {this.state.showPersons ?
+          <div className="container">
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} /> <hr />
+            <button onClick={this.switchNameHandler} className='btn btn-primary'>Switch Name</button>
+          </div> : null
+        }
+
       </div>
     )
   }
@@ -43,15 +53,48 @@ class App extends Component {
 
 export default App;
 
+// import Person from "./components/Person";
+// import { useState } from 'react'
+
 // const App = () => {
 //   // JavaScript
+//   // const [initialState, setInitialState] = useState({
+//   //   persons: [
+//   //     { name: 'Johnnathan', age: '30' },
+//   //     { name: 'Daniel', age: '27' },
+//   //     { name: 'Tony', age: '32 ' }
+//   //   ],
+//   //   someOtherStateValue: 'This is an another state value'
+//   // })
+
+//   const [personState, setPersonState] = useState([
+//     { name: 'Johnnathan', age: '30' },
+//     { name: 'Daniel', age: '27' },
+//     { name: 'Tony', age: '32 ' }
+//   ])
+
+//   const [otherStateValue, setOtherStateValue] = useState('This is an another state value')
+
+//   const changeNameHandler = () => {
+//     setPersonState([
+//       { name: 'John', age: '30' },
+//       { name: 'Tom', age: '27' },
+//       { name: 'Tony Kakkar', age: '32 ' }
+//     ])
+//   }
+
 //   return (
 //     // JSX Expression
 //     <div>
 //       <div className="container-fluid">
-//         <h1 className="page-header">Person Management Application</h1>
+//         <h1 className="page-header">Person Management</h1>
 //       </div>
-//       <div className="container"></div>
+//       <div className="container">
+//         <Person name={personState[0].name} age={personState[0].age} />
+//         <Person name={personState[1].name} age={personState[1].age} />
+//         <Person name={personState[2].name} age={personState[2].age} /> <hr />
+//         <button onClick={changeNameHandler} className='btn btn-primary'>Switch Name</button>
+//       </div>
 //     </div>
 //   );
 // }
